c6426ca80643605e5f2ccf61177d6cc3697b0d49
shybyte <shybyte@users.noreply.github.com>
2022-12-05 07:20:41 +0100

    Refactor template

--- day0x/day0x.ts
+++ day0x/day0x.ts
@@ -1,11 +1,9 @@
 export function solvePart1(input: string): number {
-  const lines = input.trim().split('\n');
-  return sum(lines.map(solvePart1SingleLine));
+  return sumMapOverLines(input, solvePart1SingleLine);
 }
 
 export function solvePart2(input: string): number {
-  const lines = input.trim().split('\n');
-  return sum(lines.map(solvePart2SingleLine));
+  return sumMapOverLines(input, solvePart2SingleLine);
 }
 
 function solvePart1SingleLine(line: string): number {
@@ -14,6 +12,11 @@ function solvePart1SingleLine(line: string): number {
 
 function solvePart2SingleLine(line: string): number {
   return line.length;
+}
+
+function sumMapOverLines(linesString: string, mapCallback: (line: string) => number): number {
+  const lines = linesString.trim().split('\n');
+  return sum(lines.map(mapCallback));
 }
 
 function sum(numbers: number[]): number {
