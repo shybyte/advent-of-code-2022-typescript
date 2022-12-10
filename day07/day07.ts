280f23f455471da1571a77f55d594f0ad08a613c
shybyte <shybyte@users.noreply.github.com>
2022-12-10 07:42:19 +0100

    day07 part 1

---
+++ day07/day07.ts
@@ -0,0 +1,121 @@
+type FileNode = Folder | File;
+
+interface Folder {
+  name: string;
+  children: FileNode[];
+  size: number;
+}
+
+interface File {
+  name: string;
+  size: number;
+}
+
+export function solvePart1(input: string): number {
+  const rootFolder = deriveFileSystem(input);
+  calculateFolderSize(rootFolder);
+  // console.log('rootFolder:', JSON.stringify(rootFolder, null, 2));
+  const allFolders = flattenFolders(rootFolder);
+  const smallFolders = allFolders.filter((folder) => folder.size <= 100_000);
+  // console.log('smallFolders:', smallFolders);
+  return sum(smallFolders.map((folder) => folder.size));
+}
+
+export function solvePart2(input: string): number {
+  return input.length;
+}
+
+function calculateFolderSize(folder: /*mut*/ Folder) {
+  folder.size = 0;
+
+  for (const child of folder.children) {
+    if (isFolder(child)) {
+      calculateFolderSize(child);
+    }
+    folder.size += child.size;
+  }
+}
+
+function flattenFolders(folder: Folder): Folder[] {
+  const result: Folder[] = [folder];
+
+  for (const child of folder.children) {
+    if (isFolder(child)) {
+      result.push(...flattenFolders(child));
+    }
+  }
+
+  return result;
+}
+
+function deriveFileSystem(input: string): Folder {
+  const rootFolder: Folder = { name: '', children: [], size: 0 };
+  const lines = input.trim().split('\n');
+  let folderStack: Folder[] = [rootFolder];
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.startsWith('$ cd')) {
+      const argument = line.slice('$ cd'.length + 1).trim();
+      folderStack = processCommandCd(argument, folderStack);
+    } else if (line.startsWith('$ ls')) {
+      i = processCommandLs(folderStack[folderStack.length - 1], lines, i);
+    } else {
+      throw new Error(`Invalid command "${line}"`);
+    }
+  }
+
+  return rootFolder;
+}
+
+function processCommandCd(argument: string, folderStack: Folder[]): Folder[] {
+  switch (argument) {
+    case '/':
+      return folderStack.slice(0, 1);
+    case '..':
+      return folderStack.slice(0, folderStack.length - 1);
+    default:
+      const currentFolder = folderStack[folderStack.length - 1];
+      const child = currentFolder.children.find((it) => it.name === argument);
+      if (child && isFolder(child)) {
+        return folderStack.concat(child);
+      } else if (child && isFile(child)) {
+        throw new Error(`cd ${argument} failed because ${argument} is not a folder but a file`);
+      } else {
+        return folderStack.concat({ name: argument, children: [], size: 0 });
+      }
+  }
+}
+
+/**
+ * Modifies currentFolder
+ * @return new inputIndex
+ */
+function processCommandLs(currentFolder: /*mut*/ Folder, inputLines: string[], currentInputIndex: number): number {
+  let i = currentInputIndex;
+  currentFolder.children = [];
+
+  while (inputLines[i + 1] && !inputLines[i + 1].startsWith('$')) {
+    i += 1;
+    const lsOutputLine = inputLines[i].split(' ');
+    if (lsOutputLine[0] === 'dir') {
+      currentFolder.children.push({ name: lsOutputLine[1], children: [], size: 0 });
+    } else {
+      currentFolder.children.push({ name: lsOutputLine[1], size: parseInt(lsOutputLine[0]) });
+    }
+  }
+
+  return i;
+}
+
+function isFolder(node: FileNode): node is Folder {
+  return 'children' in node;
+}
+
+function isFile(node: FileNode): node is File {
+  return !isFolder(node);
+}
+
+function sum(numbers: number[]): number {
+  return numbers.reduce((tempSum, val) => tempSum + val, 0);
+}
