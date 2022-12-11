4bf05e8d2648cdf269f9edd180598268307a064c
shybyte <shybyte@users.noreply.github.com>
2022-12-11 01:17:08 +0100

    day08 part2

--- day08/day08.test.ts
+++ day08/day08.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from 'bun:test';
+import { beforeEach, describe, expect, it, test } from 'bun:test';
 import { readFileSync } from 'fs';
-import { solvePart1, solvePart2 } from './day08';
+import parseMap, { calculateScenicScore, solvePart1, solvePart2 } from './day08';
 
 test('simple', () => {
   const input = readFileSync(import.meta.dir + '/test-data-simple.txt', 'utf8');
@@ -8,8 +8,8 @@ test('simple', () => {
   const solution1 = solvePart1(input);
   expect(solution1).toBe(21);
 
-  // const solution2 = solvePart2(input);
-  // expect(solution2).toBe(24933642);
+  const solution2 = solvePart2(input);
+  expect(solution2).toBe(8);
 });
 
 test('large', () => {
@@ -18,6 +18,11 @@ test('large', () => {
   const solution1 = solvePart1(input);
   expect(solution1).toBe(1818);
 
-  // const solution2 = solvePart2(input);
-  // expect(solution2).toBe(1117448);
+  const solution2 = solvePart2(input);
+  expect(solution2).toBe(368368);
+});
+
+it('calculateScenicScore should work for simple examples', () => {
+  const map = parseMap(readFileSync(import.meta.dir + '/test-data-simple.txt', 'utf8'));
+  expect(calculateScenicScore(map, 2, 1)).toBe(4);
 });
--- day08/day08.ts
+++ day08/day08.ts
@@ -1,8 +1,5 @@
 export function solvePart1(input: string): number {
-  const map = input
-    .trim()
-    .split('\n')
-    .map((s) => Array.from(s.trim(), (s) => parseInt(s)));
+  const map = parseMap(input);
   const height = map.length;
   const width = map[0].length;
 
@@ -47,6 +44,60 @@ export function solvePart1(input: string): number {
   return visibleTrees.size;
 }
 
+export default function parseMap(input: string): number[][] {
+  return input
+    .trim()
+    .split('\n')
+    .map((s) => Array.from(s.trim(), (s) => parseInt(s)));
+}
+
 export function solvePart2(input: string): number {
-  return input.length;
+  const map = parseMap(input);
+  const height = map.length;
+  const width = map[0].length;
+
+  let maxScenicScore = -1;
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      maxScenicScore = Math.max(maxScenicScore, calculateScenicScore(map, x, y));
+    }
+  }
+
+  return maxScenicScore;
+}
+
+export function calculateScenicScore(map: number[][], x: number, y: number): number {
+  const upSteps = treesUntilBlockingTreeOrBorder(map, x, y, 0, -1);
+  const leftSteps = treesUntilBlockingTreeOrBorder(map, x, y, -1, 0);
+  const rightSteps = treesUntilBlockingTreeOrBorder(map, x, y, 1, 0);
+  const downSteps = treesUntilBlockingTreeOrBorder(map, x, y, 0, 1);
+  return downSteps * upSteps * leftSteps * rightSteps;
+}
+
+export function treesUntilBlockingTreeOrBorder(
+  map: number[][],
+  xStart: number,
+  yStart: number,
+  xDelta: number,
+  yDelta: number,
+): number {
+  const originValue = map[yStart][xStart];
+  let i = 1;
+
+  while (true) {
+    const y = yStart + i * yDelta;
+    const x = xStart + i * xDelta;
+    const value = map[y]?.[x];
+
+    if (value === undefined) {
+      return i - 1;
+    }
+
+    if (value >= originValue) {
+      return i;
+    }
+
+    i += 1;
+  }
 }
